c86996416cad186221df679e8176441ddd7c5abd
Ezequiel <ezequiel3434@gmail.com>
2020-07-12 17:19:07 -0300

    constraints

--- SwooshApp/ViewController.swift
+++ SwooshApp/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgScreen: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+      
+        
     }
 
 
