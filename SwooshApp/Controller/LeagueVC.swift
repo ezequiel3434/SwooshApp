c1feef88cded45a30c20d54a17e41a15aa0f68f7
Ezequiel <ezequiel3434@gmail.com>
2020-07-12 20:21:46 -0300

    creating IBActions for buttons in LeagueVC

--- SwooshApp/Controller/LeagueVC.swift
+++ SwooshApp/Controller/LeagueVC.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    var player: Player!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        player = Player()
         // Do any additional setup after loading the view.
     }
     
@@ -21,6 +26,22 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-   
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
 
+    }
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
 }
---
+++ SwooshApp/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  SwooshApp
+//
+//  Created by Ezequiel Parada Beltran on 12/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
