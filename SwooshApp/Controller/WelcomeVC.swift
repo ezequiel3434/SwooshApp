ba17fe569c41fc1d48c4dde1ab8284ec09dee378
Ezequiel <ezequiel3434@gmail.com>
2020-07-12 19:23:45 -0300

    Creating UIVC for each one and organization the project in MVC architecture

---
+++ SwooshApp/Controller/WelcomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SwooshApp
+//
+//  Created by Ezequiel Parada Beltran on 11/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgScreen: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+      
+        
+    }
+    
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
